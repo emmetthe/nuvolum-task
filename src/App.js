f379a307cc12e5516f48b303cf213e954ed242da
Emmett He <emmetthe26@gmail.com>
2022-01-29 21:44:17 -0800

    fix toggle on chevron

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
-import Item from './components/item'
+import Item from './components/item';
 
 function App() {
   return (
     <div className="App">
       <h1>Header</h1>
+      <div className='header-desc'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis architecto neque illum eius error, totam mollitia reiciendis
+        officiis quis magnam?
+      </div>
       <Item />
     </div>
   );
--- src/components/item.js
+++ src/components/item.js
@@ -2,13 +2,15 @@ import React from 'react';
 import axios from 'axios';
 import './item.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
+import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
+import StarRatingComponent from 'react-star-rating-component';
 
 class Item extends React.Component {
   state = {
     items: [],
     showDetails: false,
-    currItem: null
+    currItem: null,
+    isActive: false
   };
 
   componentDidMount() {
@@ -19,7 +21,7 @@ class Item extends React.Component {
   }
 
   handleDetails(item) {
-    this.setState({ showDetails: !this.state.showDetails, currItem: item });
+    this.setState({ showDetails: !this.state.showDetails, currItem: item, isActive: !this.state.isActive });
   }
 
   render() {
@@ -27,11 +29,16 @@ class Item extends React.Component {
     if (this.state.showDetails) {
       itemDetails = (
         <div className="item-detail-container">
-          <div>${this.state.currItem.price}</div>
-          <div>{this.state.currItem.description}</div>
-          <div>{this.state.currItem.rating.rate}</div>
-          <div>({this.state.currItem.rating.count})</div>
-          <button>Add to cart</button>
+          <div className="top-details">
+            <div className="item-price">${this.state.currItem.price}</div>
+            <div>{this.state.currItem.description}</div>
+            <div className="item-ratings">
+              {/* <div>{this.state.currItem.rating.rate}</div> */}
+              <StarRatingComponent name="rate1" starCount={5} value={this.state.currItem.rating.rate} onStarClick={null} />
+              <div>({this.state.currItem.rating.count})</div>
+            </div>
+          </div>
+          <button className="cart-button">Add to cart</button>
         </div>
       );
     }
@@ -42,7 +49,10 @@ class Item extends React.Component {
             <div className="item" key={item.id} onClick={() => this.handleDetails(item)}>
               <img className="item-image" src={`${item.image}`}></img>
               <p className="item-title">{item.title}</p>
-              <FontAwesomeIcon className="chevron-right" icon={faChevronRight} />
+              <FontAwesomeIcon
+                className={this.state.isActive && this.state.currItem == item ? 'chevron-right-active' : 'chevron-left'}
+                icon={this.state.isActive && this.state.currItem == item ? faChevronRight : faChevronLeft}
+              />
             </div>
           ))}
         </div>
